219b8e2436c5410b90a629e7c088b6129a09f797
abhi-kr-210 <abhishekk1.ug21.ec@nitp.ac.in>
2025-01-30 02:59:26 +0530

    api changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ import axios from "axios"
 function App() {
   const [jokes, setjokes] = useState([])
    useEffect(()=>{
-       axios.get("https://funproj-backendr.onrender.com/api/jokes")
+       axios.get("/api/jokes")
        .then((res)=>{
            setjokes(res.data);
        })
